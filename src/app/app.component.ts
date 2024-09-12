4c19d4785025b4105b6fa5edb1e6c5f6808ec10e
ngocan01676 <ngocan01676@gmail.com>
2024-09-12 22:11:02 +0700

    update code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NzTableModule } from 'ng-zorro-antd/table';
+interface ItemData {
+  name: string;
+  age: number;
+  address: string;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NzTableModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo-antdesign';
+  listOfData: ItemData[] = [];
+
+  ngOnInit(): void {
+    for (let i = 0; i < 100; i++) {
+      this.listOfData.push({
+        name: `Edward King ${i}`,
+        age: 32,
+        address: `London`
+      });
+    }
+  }
 }
